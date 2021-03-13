7662d50da36695a33edddcd993f802c5d53b4ffe
Jahirhdz <jahirhdz@hotmail.com>
2021-03-12 22:09:47 -0800

    Database setup

--- application/app.js
+++ application/app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+if(process.env.NODE_ENV === 'development') {
+  require("dotenv").config();
+}
+
 var app = express();
 
 // view engine setup
